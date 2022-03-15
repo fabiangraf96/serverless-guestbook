344092edaad6aff791904dcb9466b3b2925deae1
fabiangraf96 <99732854+fabiangraf96@users.noreply.github.com>
2022-03-15 09:40:49 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://3bc29e14.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
